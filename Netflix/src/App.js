95898106c61cca5ab2300c0c4c4804408f46ee48
deepika <deepikabajaj31@gmail.com>
2022-07-13 02:58:03 +0530

    Done

--- Netflix/src/App.js
+++ Netflix/src/App.js
@@ -1,10 +1,10 @@
-
+import SignOut from "./components/SignOut/SignOut";
 import Signup from "./components/SignUp/Signup";
 // import Terms from "./components/SignUp/Terms";
 function App() {
   return (
     <div>
-<Signup/>
+<SignOut/>
 {/* <Terms/> */}
 {/* signup form validation not working */}
     </div>
--- Netflix/src/components/UI/Head.js
+++ Netflix/src/components/UI/Head.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import logo from '../../images/logo.svg';
 import classes from './Head.module.css'
-const Head = () => {
-    return (
-        <div className={classes.page}>
-            <img alt='error' src={logo} className={classes.image} />
-            <a href='/login' className={classes.name}>Sign In</a>
-        </div>
-    )
+const Head=()=>{
+return (
+    <div className={classes.page}>
+        <img alt='error'src={logo} className={classes.image}/>
+        <a href='/login' className={classes.name}>Sign In</a>
+    </div>
+)
 }
 export default Head;
